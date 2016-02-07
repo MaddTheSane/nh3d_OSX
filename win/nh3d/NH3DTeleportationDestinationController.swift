ee33f93d8555d12cb98e9f7cb099edf588483a9a
C.W. Betts <computers57@hotmail.com>
2016-02-06 23:28:25 -0700

    Initial work on having a selectable map for teleportation.

---
+++ win/nh3d/NH3DTeleportationDestinationController.swift
@@ -0,0 +1,19 @@
+//
+//  NH3DTeleportationDestinationController.swift
+//  NetHack3D
+//
+//  Created by C.W. Betts on 2/6/16.
+//  Copyright © 2016 Haruumi Yoshino. All rights reserved.
+//
+
+import Cocoa
+
+class NH3DTeleportationDestinationController: NSWindowController {
+	@IBOutlet weak var scrollView: NSScrollView!
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
